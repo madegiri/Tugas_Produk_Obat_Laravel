bdb5a39eb12d501fdb944dbb45dc3b3ead81d2bd
madegiri <madedharmagiri6@gmail.com>
2024-06-11 00:42:22 +0700

    update nama brand

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,7 +9,7 @@
 <body class="bg-light">
     <nav class="navbar navbar-expand-lg navbar-dark" style="background: linear-gradient(to left, #343a40, #6c757d);">
         <div class="container">
-          <a class="navbar-brand" href="#">Produk Obat</a>
+          <a class="navbar-brand" href="#">Apotek Dharma</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
